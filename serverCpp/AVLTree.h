ac0a98e6f84c6c7a75a894657930943c723cc03e
Shimon <sh0527691229@gmail.com>
2024-08-19 15:31:22 +0300

    add avl Tree temlate class

--- serverCpp/AVLTree.h
+++ serverCpp/AVLTree.h
@@ -1,8 +1,24 @@
 #pragma once
+#include "Node.h"
 
-
-
+template <typename T>
 class AVLTree
 {
+private:
+	Node<T>* root;
+public:
+	AVLTree();
+	void insertToTree(T newItem);
 };
 
+template<typename T>
+inline AVLTree<T>::AVLTree()
+	: root(nullptr)
+{
+}
+
+template<typename T>
+inline void AVLTree<T>::insertToTree(T newItem)
+{
+	root = insertToTree(root, newItem);
+}
